66a65228eb49b5e6339cc7a934fa30db8a2ec8ee
LB <luigi.bungaro@gac.com>
2024-10-06 13:18:52 +0400

    changinh streamlit set up

--- pages/0_Rent_transactions_overtime.py
+++ pages/0_Rent_transactions_overtime.py
@@ -1,7 +1,10 @@
 import streamlit as st
 from streamlit.logger import get_logger
-from utils import load_rent_df
 import numpy as np
+from Hello import duckCon
+import plotly.express as px
+import pandas as pd
+
 
 LOGGER = get_logger(__name__)
 
@@ -10,62 +13,129 @@ def filter_year(df, year):
     return df[df["year"]==year] 
 
 def run():
-    st.set_page_config(
-        page_title="Home",
-        page_icon="👋",
-        layout="wide",
-        initial_sidebar_state="expanded"
-    )
+    # st.set_page_config(
+    #     page_title="Home",
+    #     page_icon="👋",
+    #     layout="wide",
+    #     initial_sidebar_state="expanded"
+    # )
     
-    df = load_rent_df()
+    df_villa_offplan_area_price= duckCon.execute("""
+                          select
+                            reg_type_en,
+                            median(procedure_area) as area,
+                            median(meter_sale_price) as meter_sale_price
+                          from main.sale_contracts
+                          where year(instance_date)=year(current_timestamp) and property_usage_en = 'Residential' and property_type_en='Villa'
+                          GROUP by reg_type_en
+                         """).fetch_df()
+    df_unit_offplan_area_price= duckCon.execute("""
+                          select
+                            reg_type_en,
+                            median(procedure_area) as area,
+                            median(meter_sale_price) as meter_sale_price
+                          from main.sale_contracts
+                          where year(instance_date)=year(current_timestamp) and property_usage_en = 'Residential' and property_type_en='Unit'
+                          GROUP by reg_type_en
+                         """).fetch_df()
+    df_monthly_rolling = duckCon.execute("select * from main_golden.rolling_monthly__yearly_sales").fetch_df()
+    df_monthly_rolling['year_month'] = pd.to_datetime(df_monthly_rolling['year_month'], format='%Y%m')
+    df_monthly_rolling = df_monthly_rolling.sort_values(by='year_month')
 
-    dubai_areas = sorted(df["area"].unique().tolist())
-    dubai_buildings = sorted(df["building_name"].unique().tolist())
-    years = sorted(df["year"].unique().tolist())[::-1]
-    builing_type = sorted(df["type"].unique().tolist())
+    # Create the plot
+    fig_reg_type = px.line(df_monthly_rolling, 
+                  x='year_month', 
+                  y=[
+                    'rolling_12_month_num_sales_offplan', 
+                    'rolling_12_month_num_sales_existing',
+                    'total_sales'], 
+                  labels={
+                      'year_month': 'Year-Month',
+                      'value': 'Sales Count',
+                      'variable': 'Rolling Metrics'
+                  },
+                  title="Monthly Rolling Sales Values by Registration Type",)
+    # Increase the size of the graph
+    fig_reg_type.update_layout(
+        width=1000,  # Increase width
+        height=600,  # Increase height
+        title_font_size=20,  # Optional: increase title size
+        xaxis_title_font_size=16,  # Optional: increase x-axis title size
+        yaxis_title_font_size=16  # Optional: increase y-axis title size
+    )
 
-    st.write("### Rent Transactions. Select desired Area of Dubai, or building name, or building type (1BR, 2BR and 3BR) or specific year of interest")
-    st.write("These data are collected from Dubai open Dataset (Dubai Pulse that has also historical data)")
-    st.write("You can select the building of interest and see at what price the last contracts were signed)")
+    # Create the plot
+    fig_prop_type = px.line(df_monthly_rolling, 
+                  x='year_month', 
+                  y=['rolling_12_month_num_sales_villa', 
+                     'rolling_12_month_num_sales_unit',
+                    'total_sales'], 
+                  labels={
+                      'year_month': 'Year-Month',
+                      'value': 'Sales Count',
+                      'variable': 'Rolling Metrics'
+                  },
+                  title="Monthly Rolling Sales Values by Property Type")
+    # Increase the size of the graph
+    fig_prop_type.update_layout(
+        width=1000,  # Increase width
+        height=600,  # Increase height
+        title_font_size=20,  # Optional: increase title size
+        xaxis_title_font_size=16,  # Optional: increase x-axis title size
+        yaxis_title_font_size=16  # Optional: increase y-axis title size
+    )
+
 
 
     with st.container():
-      col1, col2, col3, col4 = st.columns(4)
-      with col1:
-        selected_areas = st.multiselect(label="Area", options=dubai_areas)
-      with col2:
-        seleceted_buildings = st.multiselect(label="Building Name", options=dubai_buildings)
-      with col3:
-        seleceted_building_type = st.multiselect(label="Building Type", options=builing_type)
-      with col4:
-        seleceted_years = st.multiselect(label="Year", options=years)
+      st.title("Dubai House Rent and Sales Prices Report")
+    with st.container():
+      col_villa, col_unit = st.columns(2)
+      with col_villa:
+        st.header("Villa")
+        st.markdown("#### Off-Plan to Existing space Growth Rate")
+        sqm_existing = round(df_villa_offplan_area_price.iloc[0,1],0)
+        sqm_offplan = round(df_villa_offplan_area_price.iloc[1,1],1)
+        sqm_offplan_vs_existing = round( (sqm_offplan-sqm_existing)/sqm_existing * 100, 1)
+        st.metric(label="", value=f"{sqm_offplan} m2", delta=f"{sqm_offplan_vs_existing} %")
+
+        st.markdown("#### Off-Plan to Existing price/sqm Growth Rate")
+        sqm_price_existing = round(df_villa_offplan_area_price.iloc[0,2],0)
+        sqm_price_offplan = round(df_villa_offplan_area_price.iloc[1,2],0)
+        sqm_price_offplan_vs_existing = round( (sqm_price_offplan - sqm_price_existing)/sqm_price_existing * 100, 1)
+        st.metric(label="", value=f"{sqm_price_offplan} AED/m2", delta=f"{sqm_price_offplan_vs_existing} %")
+      with col_unit:
+        st.header("Unit")
+        st.markdown("#### Off-Plan to Existing space Growth Rate")
+        sqm_existing = round(df_unit_offplan_area_price.iloc[0,1],0)
+        sqm_offplan = round(df_unit_offplan_area_price.iloc[1,1],1)
+        sqm_offplan_vs_existing = round( (sqm_offplan-sqm_existing)/sqm_existing * 100, 1)
+        st.metric(label="", value=f"{sqm_offplan} m2", delta=f"{sqm_offplan_vs_existing} %")
+
+        st.markdown("#### Off-Plan to Existing price/sqm Growth Rate")
+        sqm_price_existing = round(df_unit_offplan_area_price.iloc[0,2],0)
+        sqm_price_offplan = round(df_unit_offplan_area_price.iloc[1,2],0)
+        sqm_price_offplan_vs_existing = round( (sqm_price_offplan - sqm_price_existing)/sqm_price_existing * 100, 1)
+        st.metric(label="", value=f"{sqm_price_offplan} AED/m2", delta=f"{sqm_price_offplan_vs_existing} %")
+
+    # Customize hover data to show values on hover
+    st.header("Rolling Sales by Registration Type: Off-Plan vs Existing")
+    fig_reg_type.update_traces(mode="lines+markers", hovertemplate='%{x}<br>Sales: %{y}<extra></extra>')
+
+    # Add chart to Streamlit app
+    st.plotly_chart(fig_reg_type)
+
+    st.header("Rolling Sales by Property Type: Villa vs Unit")
+    fig_prop_type.update_traces(mode="lines+markers", hovertemplate='%{x}<br>Sales: %{y}<extra></extra>')
+
+    # Add chart to Streamlit app
+    st.plotly_chart(fig_prop_type)
 
 
     st.sidebar.success("Select page here.")
 
 
-    with st.container():
-      rows_per_page = 1000
-      total_pages = (len(df)-1) // rows_per_page + 1
-      # Get the current page number from the user
-      page_number = st.multiselect(label="Page Number", options=list(np.arange(1,total_pages, 1)))
-      #page_number = st.slider("Select Page", 1, total_pages, 1)
-      if not page_number:
-         page_number = [1]
 
-      start_index = (int(page_number[0]) - 1) * rows_per_page
-      end_index = min(int(page_number[0]) * rows_per_page, len(df))
-      if selected_areas:
-        df = df[df["area"].isin(selected_areas)]
-      if seleceted_buildings:
-        df = df[df["building_name"].isin(seleceted_buildings)]
-      if seleceted_years:
-          df = df[df["year"].isin(seleceted_years)]
-      if seleceted_building_type:
-          df = df[df["type"].isin(seleceted_building_type)]
-      st.dataframe(
-          df.iloc[start_index:end_index]
-      )
 
 
 if __name__ == "__main__":
--- pages/1_Number_of_rent_contracts_overtime.py
+++ pages/1_Number_of_rent_contracts_overtime.py
@@ -6,28 +6,28 @@ from streamlit.hello.utils import show_code
 from utils import load_rent_df
 import plotly.express as px
 import datetime
+from Hello import duckCon
 
 def plotting_num_transactions():
     st.set_page_config(
         page_title="Yearly Transactions",
         layout="wide"
     )
-    df = load_rent_df()
+    df = duckCon.execute("SELECT * FROM main_golden.monthly_rent").fetch_df()
+    df["year"] = df["year_month"].str[:4]
 
     months_list = []
     today = datetime.datetime.now()
     for i in range(1,25):
-        year_month = (datetime.datetime(today.year, today.month, 1) - datetime.timedelta(20*i)).strftime("%Y-%m")
+        year_month = (datetime.datetime(today.year, today.month, 1) - datetime.timedelta(20*i)).strftime("%Y%m")
         months_list.append(year_month)
     # plotting only last year data
-    df["year_month"] = df["year_month"].astype(str)
-    group_area_1y = df[df["year_month"].isin(months_list)].groupby(["year_month", "type"])["year"].count().reset_index()
-    group_area_1y["year_month"] = pd.to_datetime(group_area_1y["year_month"].astype(str))
-
+    group_area_1y = df[df["year_month"].isin(months_list)].groupby(["year_month", "year"]).count().reset_index()
+    # group_area_1y["year_month"] = pd.to_datetime(group_area_1y["year_month"].astype(str))
 
     # Create an interactive line plot using plotly express
     fig1 = px.line(group_area_1y,
-                x='year_month', y='year', color='type', markers=True,
+                x='year_month', y='year', markers=True, #color='type',
                 title='Last 2 years Transactions', labels={'year_month': 'Num transactions'})
     fig1.update_layout(width=1400, height=400)
 
@@ -36,13 +36,13 @@ def plotting_num_transactions():
 
 
 
-    group_area = df.groupby(["year", "type"])["year_month"].count().reset_index()
+    group_area = df.groupby(["year"])["year_month"].count().reset_index()
     group_area["year"] = pd.to_datetime(group_area["year"].astype(str))
     group_area.head(2)
 
     # Create an interactive line plot using plotly express
     fig2 = px.line(group_area,
-                x='year', y='year_month', color='type', markers=True,
+                x='year', y='year_month', markers=True, #color='type', 
                 title='Num transactions Over Time', labels={'year_month': 'Num transactions'})
     fig2.update_layout(width=1400, height=400)
 
