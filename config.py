df367bd4c4db024c50c1eab92ec259e8cd93dbed
LB <luigi.bungaro@gac.com>
2024-11-02 06:56:03 +0400

    updated paths

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-file_path_rent_csv = "/Users/luigibungaro/personal/dubai-house-sell-rent-prices/data/Rent_Contracts.csv"
-file_path_sell_csv = "/Users/luigibungaro/personal/dubai-house-sell-rent-prices/data/Transactions.csv"
+file_path_rent_csv = "/Users/luigibungaro/Library/CloudStorage/OneDrive-GulfAgencyCompany/files_macbook/data/dubai-house-market/Rent_Contracts.csv"
+file_path_sell_csv = "/Users/luigibungaro/Library/CloudStorage/OneDrive-GulfAgencyCompany/files_macbook/data/dubai-house-market/Transactions.csv"
 
-file_path_rent_parquet = "/Users/luigibungaro/personal/dubai-house-sell-rent-prices/data/Rent_Contracts.parquet"
-file_path_sell_parquet = "/Users/luigibungaro/personal/dubai-house-sell-rent-prices/data/Transactions.parquet"
+file_path_rent_parquet = "/Users/luigibungaro/Library/CloudStorage/OneDrive-GulfAgencyCompany/files_macbook/data/dubai-house-market/Rent_Contracts.parquet"
+file_path_sell_parquet = "/Users/luigibungaro/Library/CloudStorage/OneDrive-GulfAgencyCompany/files_macbook/data/dubai-house-market/Transactions.parquet"
 
-duckdb_path = "/Users/luigibungaro/personal/dubai-house-sell-rent-prices/dubai_housing.duckdb"
+duckdb_path = "/Users/luigibungaro/Library/CloudStorage/OneDrive-GulfAgencyCompany/files_macbook/data/duck_databases/dubai-house-market.db"
